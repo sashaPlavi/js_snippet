2260850156b8adf307d3e1e072db7d350d811290
SashaPlavi <sasha.bingulac@gmail.com>
2020-06-02 23:15:02 +0200

    poping

--- stack.js
+++ stack.js
@@ -11,6 +11,28 @@ class Stack {
 
     return this.count;
   }
+  pop() {
+    if (this.items.length === 0) return undefined;
+    const pop = this.items[this.count - 1];
+    this.count--;
+
+    if (this.count === -1) {
+      this.items = [];
+    }
+    let newitems = [];
+    for (let i = 0; i <= this.count; i++) {
+      newitems[i] = this.items[i];
+      this.items = newitems;
+    }
+
+    return pop;
+  }
+  peek() {
+    return this.items[this.count - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0 ? true : false;
+  }
 }
 
 const stack = new Stack();
@@ -18,3 +40,10 @@ const stack = new Stack();
 stack.push(100);
 stack.push(200);
 stack.push(300);
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+
+console.log(stack.isEmpty());
